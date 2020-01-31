1d33a71873d7f2170a0376cbc42296455de36741
waheealam <walam@unomaha.edu>
2020-01-31 03:46:32 -0600

    Sql Lite Commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,12 +76,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfoliodb1',
-        'USER':'postgres',
-        'PASSWORD':'amin2009',
-        'HOST':'localhost',
-        'PORT':'5432',
+'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'portfoliodb1',
+        #'USER':'postgres',
+        #'PASSWORD':'amin2009',
+        #'HOST':'localhost',
+        #'PORT':'5432',
 
     }
 }
